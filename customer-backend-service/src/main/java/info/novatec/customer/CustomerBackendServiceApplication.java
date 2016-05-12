0f0e7c682c4d9bcbc0aec42dd1efacbb6c570fbb
Andreas Falk <andreas.falk@novatec-gmbh.de>
2016-05-12 18:13:20 +0200

    Initial commit

---
+++ customer-backend-service/src/main/java/info/novatec/customer/CustomerBackendServiceApplication.java
@@ -0,0 +1,12 @@
+package info.novatec.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerBackendServiceApplication.class, args);
+	}
+}
---
+++ item-backend-service/src/main/java/info/novatec/item/ItemBackendServiceApplication.java
@@ -0,0 +1,12 @@
+package info.novatec.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemBackendServiceApplication.class, args);
+	}
+}
